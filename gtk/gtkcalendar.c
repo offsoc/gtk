679d5079694e17652a6d88bd93fe9763c11cb814
Arjan Molenaar <gaphor@gmail.com>
2024-05-22 12:04:39 +0200

    macos: keybindings for GtkCalendar
    Use Cmd, instead of Ctrl on macOS.

--- gtk/gtkcalendar.c
+++ gtk/gtkcalendar.c
@@ -1374,6 +1374,11 @@ gtk_calendar_key_controller_key_pressed (GtkEventControllerKey *controller,
   int old_focus_row;
   int old_focus_col;
   int row, col, day;
+#ifdef __APPLE__
+  GdkModifierType modifier = GDK_META_MASK;
+#else
+  GdkModifierType modifier = GDK_CONTROL_MASK;
+#endif
 
   return_val = FALSE;
 
@@ -1385,7 +1390,7 @@ gtk_calendar_key_controller_key_pressed (GtkEventControllerKey *controller,
     case GDK_KEY_KP_Left:
     case GDK_KEY_Left:
       return_val = TRUE;
-      if (state & GDK_CONTROL_MASK)
+      if (state & modifier)
         calendar_set_month_prev (calendar);
       else
         {
@@ -1397,7 +1402,7 @@ gtk_calendar_key_controller_key_pressed (GtkEventControllerKey *controller,
     case GDK_KEY_KP_Right:
     case GDK_KEY_Right:
       return_val = TRUE;
-      if (state & GDK_CONTROL_MASK)
+      if (state & modifier)
         calendar_set_month_next (calendar);
       else
         {
@@ -1409,7 +1414,7 @@ gtk_calendar_key_controller_key_pressed (GtkEventControllerKey *controller,
     case GDK_KEY_KP_Up:
     case GDK_KEY_Up:
       return_val = TRUE;
-      if (state & GDK_CONTROL_MASK)
+      if (state & modifier)
         calendar_set_year_prev (calendar);
       else
         {
@@ -1421,7 +1426,7 @@ gtk_calendar_key_controller_key_pressed (GtkEventControllerKey *controller,
     case GDK_KEY_KP_Down:
     case GDK_KEY_Down:
       return_val = TRUE;
-      if (state & GDK_CONTROL_MASK)
+      if (state & modifier)
         calendar_set_year_next (calendar);
       else
         {
